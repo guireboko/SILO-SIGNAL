7e4515e69aa57defdb5a82a42bee067b416fa7ec
ShirleyFerr <shirley.lima@sptech.school>
2024-10-07 21:38:39 -0300

    criação das tabelas empresa, silo e sensores no script

--- BD/SCRIPT BD USUARIO.sql
+++ BD/SCRIPT BD USUARIO.sql
@@ -14,3 +14,40 @@ CREATE TABLE usuario (
     CONSTRAINT fk_usuario_responsavel
         FOREIGN KEY (fkResponsavel) REFERENCES usuario(idusuario)
 );
+
+create table empresa (
+	idEmpresa int primary key auto_increment,
+    nome varchar(45),
+    cnpj char(14),
+    cep char(9),
+    cidade varchar(45),
+    numero char(8),
+    fk_responsavel int,
+    constraint fkResposavelEmpresa 
+		foreign key (fk_responsavel) references usuario(idusuario)
+);
+
+create table silo (
+	idSilo int primary key auto_increment,
+    qtdAtual decimal (9, 2),
+    capacidadeMaxima decimal (9, 2),
+    responsavelVistoria varchar(45),
+    dtUltimaVistoria date, 
+    fk_empresa int,
+    constraint fkEmpresaResponsavelSilo
+		foreign key (fk_empresa) references empresa(idEmpresa) 
+);
+
+create table sensores (
+	idSensores int primary key auto_increment,
+    dtImplementacao date,
+    dtUltimaManutencao date,
+    responsavelManutencao varchar(45),
+    porcentagemDetec float (3, 1),
+    instanteDetec datetime,
+    perigoBandeira varchar(8),
+    constraint ckhBandeira check (perigoBandeira in('Verde', 'Amarela', 'Vermelha')),
+    fk_silo int, 
+    constraint fkSiloSensor 
+		foreign key (fk_silo) references silo(idSilo)
+);
